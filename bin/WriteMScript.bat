f5dcfb2dd281c7504361ad7f7286d640b7c2b8e3
MicomScript Team <121068650+micomscript@users.noreply.github.com>
2024-03-07 10:30:35 +0900

    Add files via upload

---
+++ bin/WriteMScript.bat
@@ -0,0 +1,1 @@
+esptool.exe --chip esp32s3 --port COM4 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0 MScript.ino.bootloader.bin 0x8000 MScript.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 MScript.ino.bin 
